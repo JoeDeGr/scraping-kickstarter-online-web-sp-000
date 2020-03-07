ed921f5d23f4abefaff0c890dfd64d3dfee9b6db
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-07 01:34:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,12 +16,10 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
+      :location => project.css("span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-  #return the projects
-  projects
 end
 
 # projects: kickstarter.css("li.project.grid_4")
